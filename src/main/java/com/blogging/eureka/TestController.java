7eb94f88b814b45f0b7d492165a27146741026a8
techone577 <1421709476@qq.com>
2019-05-08 12:37:23 +0800

    blogging-1.0

--- src/main/java/com/blogging/eureka/TestController.java
+++ src/main/java/com/blogging/eureka/TestController.java
@@ -3,7 +3,6 @@ package com.blogging.eureka;
 import com.blogging.eureka.model.eureka.ServiceInstance;
 import com.blogging.eureka.support.eureka.EurekaInstanceCache;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,19 +16,19 @@ import java.util.List;
 @RequestMapping("/test")
 public class TestController {
 
+////    @Autowired
+////    RestTemplate restTemplate;
+//
 //    @Autowired
-//    RestTemplate restTemplate;
-
-    @Autowired
-    DiscoveryClient client;
-
-    @RequestMapping("/rest")
-    public String restTest () {
-//        RestTemplate restTemplate = new RestTemplate();
-//        ResponseEntity<String> a = restTemplate.getForEntity("http://localhost:1001/eureka/apps", String.class);
-//        JSONObject jsonObject = JSONObject.parseObject(a.getBody());
-//        String str = jsonObject.getJSONObject("applications").getString("application");
-        List<ServiceInstance> si = EurekaInstanceCache.getInstanceCache();
-        return "null";
-    }
+//    DiscoveryClient client;
+//
+//    @RequestMapping("/rest")
+//    public String restTest () {
+////        RestTemplate restTemplate = new RestTemplate();
+////        ResponseEntity<String> a = restTemplate.getForEntity("http://localhost:1001/eureka/apps", String.class);
+////        JSONObject jsonObject = JSONObject.parseObject(a.getBody());
+////        String str = jsonObject.getJSONObject("applications").getString("application");
+//        List<ServiceInstance> si = EurekaInstanceCache.getInstanceCache();
+//        return "null";
+//    }
 }
